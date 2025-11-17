5345cabbbe9ded04e593c36d6df6eb7c3b96f675
Melvyn de Kort <melvyn@mdekort.nl>
2025-11-17 17:57:29 +0100

    Add Lambda deployment notifications

--- notifications-monitoring.tf
+++ notifications-monitoring.tf
@@ -74,3 +74,21 @@ resource "aws_cloudwatch_event_target" "acm_expiration" {
   arn       = aws_sns_topic.notifications.arn
   role_arn  = aws_iam_role.eventbridge_notifications.arn
 }
+
+# Lambda function updates
+resource "aws_cloudwatch_event_rule" "lambda_deployments" {
+  name        = "lambda-deployment-notifications"
+  description = "Forward Lambda function updates to notifications"
+
+  event_pattern = jsonencode({
+    "source" : ["aws.lambda"],
+    "detail-type" : ["Lambda Function Update"]
+  })
+}
+
+resource "aws_cloudwatch_event_target" "lambda_deployments" {
+  rule      = aws_cloudwatch_event_rule.lambda_deployments.name
+  target_id = "NotificationsLambdaTarget"
+  arn       = aws_sns_topic.notifications.arn
+  role_arn  = aws_iam_role.eventbridge_notifications.arn
+}

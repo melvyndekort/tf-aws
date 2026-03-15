2781fbd663fa22b3054476891c52a929b0266f93
Melvyn de Kort <melvyn@mdekort.nl>
2026-03-15 13:22:57 +0100

    Add S3 encryption to bootstrap bucket, improve subaccount CI

--- modules/account-bootstrap/main.tf
+++ modules/account-bootstrap/main.tf
@@ -137,3 +137,12 @@ resource "aws_s3_bucket_public_access_block" "tfstate" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "tfstate" {
+  bucket = aws_s3_bucket.tfstate.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
